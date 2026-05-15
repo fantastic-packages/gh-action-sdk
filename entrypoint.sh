4fa05f82c677ebac5fb422072628961927a8d480
Anya Lin <hukk1996@gmail.com>
2026-05-15 13:16:01 +0800

    Merge remote-tracking branch 'openwrt/main'
    (cherry picked from commit b6caaf62c20a7328d71149bfcd0d4cd63a12f4d2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,6 +42,8 @@ done
 [ -z "$DL_DIR" ] || { rm -rf dl; ln -s "$DL_DIR" dl; }
 
 FEEDNAME="${FEEDNAME:-action}"
+# Build requested packages by default, otherwise just check
+BUILD="${BUILD:-1}"
 BUILD_LOG="${BUILD_LOG:-1}"
 
 # opkg key-build
@@ -131,19 +133,21 @@ else
 			"package/$PKG/download" V=s
 		endgroup
 
-		group "make package/$PKG/check"
+		[ "$BUILD" = '1' ] && group "make package/$PKG/check"
 		make \
 			BUILD_LOG="$BUILD_LOG" \
 			IGNORE_ERRORS="$IGNORE_ERRORS" \
 			"package/$PKG/check" V=s 2>&1 | \
 				tee logtmp
-		endgroup
 
 		RET=${PIPESTATUS[0]}
+		[ "$BUILD" = '1' ] && endgroup
 
 		if [ "$RET" -ne 0 ]; then
-			echo_red   "=> Package check failed: $RET)"
+			echo 'Package check failed'
 			exit "$RET"
+		elif [ "$BUILD" = 0 ]; then
+			echo 'Package check successful'
 		fi
 
 		badhash_msg="HASH does not match "
@@ -156,12 +160,12 @@ else
 
 		PATCHES_DIR=$(find $feed_dir -path "*/$PKG/patches")
 		if [ -d "$PATCHES_DIR" ] && [ -z "$NO_REFRESH_CHECK" ]; then
-			group "make package/$PKG/refresh"
+			[ "$BUILD" = '1' ] && group "make package/$PKG/refresh"
 			make \
 				BUILD_LOG="$BUILD_LOG" \
 				IGNORE_ERRORS="$IGNORE_ERRORS" \
 				"package/$PKG/refresh" V=s
-			endgroup
+			[ "$BUILD" = '1' ] && endgroup
 
 			if ! git -C "$PATCHES_DIR" diff --quiet -- .; then
 				echo "Dirty patches detected, please refresh and review the diff"
@@ -186,8 +190,12 @@ else
 				exit 1
 			fi
 		fi
-
 	done
+
+	if [ "$BUILD" != '1' ]; then
+		echo 'Skipping build'
+		exit
+	fi
 
 	make \
 		-f .config \
